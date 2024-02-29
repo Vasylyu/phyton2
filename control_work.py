cc193d4876651bb59e17821bc40fedc11ffdaf17
Vasy <taki003@yandex.ru>
2024-03-01 00:50:36 +0700

    save_two

--- control_work.py
+++ control_work.py
@@ -1,3 +1,8 @@
 n = 1
 string = [0] * n
 string = (input('Введите массив: '))
+for i in range(n):
+   print(string)
+word_list = string.split(",")
+count = len(word_list)
+print("количество введенных строк:", count)
